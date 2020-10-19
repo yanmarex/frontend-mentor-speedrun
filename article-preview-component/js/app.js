2c9725357fb2f55588a670744724f31c2ba83fd1
yanmarex <60897746+yanmarex@users.noreply.github.com>
2020-10-19 19:24:38 -0400

    Add share btn actions

--- article-preview-component/js/app.js
+++ article-preview-component/js/app.js
@@ -1,1 +1,11 @@
+//First button
 const shareBtn = document.getElementById("share");
+const popup = document.getElementById("popup");
+shareBtn.addEventListener("click", () => {
+  popup.classList.toggle("is-visible");
+});
+//Second button
+const shareBtnSecond = document.getElementById("second-popup");
+shareBtnSecond.addEventListener("click", () => {
+  popup.classList.toggle("is-visible");
+});
